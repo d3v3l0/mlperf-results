647c49987ad672c40f9a82140452640e87ebc344
Deepak Narayanan <deepakn94@gmail.com>
2019-02-14 21:36:56 +0000

    Update to ompi_bind_DGX1.sh

--- v0.5.0/nvidia/submission/code/image_classification/mxnet/ompi_bind_DGX1.sh
+++ v0.5.0/nvidia/submission/code/image_classification/mxnet/ompi_bind_DGX1.sh
@@ -3,35 +3,35 @@
 case "${OMPI_COMM_WORLD_LOCAL_RANK}" in
 0)
     export OMPI_MCA_btl_openib_if_include=mlx5_0
-    exec numactl --physcpubind=0-4,40-44 --membind=0 $@
+    exec numactl --physcpubind=0-3,32-35 --membind=0 $@
     ;;
 1)
     export OMPI_MCA_btl_openib_if_include=mlx5_0
-    exec numactl --physcpubind=5-9,45-49 --membind=0 $@
+    exec numactl --physcpubind=4-7,36-39 --membind=0 $@
     ;;
 2)
     export OMPI_MCA_btl_openib_if_include=mlx5_1
-    exec numactl --physcpubind=10-14,50-54 --membind=0 $@
+    exec numactl --physcpubind=8-11,40-43 --membind=0 $@
     ;;
 3)
     export OMPI_MCA_btl_openib_if_include=mlx5_1
-    exec numactl --physcpubind=15-19,55-59 --membind=0 $@
+    exec numactl --physcpubind=12-15,44-47 --membind=0 $@
     ;;
 4)
     export OMPI_MCA_btl_openib_if_include=mlx5_2
-    exec numactl --physcpubind=20-24,60-64 --membind=1 $@
+    exec numactl --physcpubind=16-19,48-51 --membind=1 $@
     ;;
 5)
     export OMPI_MCA_btl_openib_if_include=mlx5_2
-    exec numactl --physcpubind=25-29,65-69 --membind=1 $@
+    exec numactl --physcpubind=20-23,52-55 --membind=1 $@
     ;;
 6)
     export OMPI_MCA_btl_openib_if_include=mlx5_3
-    exec numactl --physcpubind=30-34,70-74 --membind=1 $@
+    exec numactl --physcpubind=24-27,56-59 --membind=1 $@
     ;;
 7)
     export OMPI_MCA_btl_openib_if_include=mlx5_3
-    exec numactl --physcpubind=35-39,75-79 --membind=1 $@
+    exec numactl --physcpubind=28-31,60-63 --membind=1 $@
     ;;
 esac
 
